4818da19c8d40d3663595a82754a1bcadb166021
shengtianzhu <shengtianzhu2008@163.com>
2018-07-24 20:44:12 +0800

    增加资源打包的编辑器功能

---
+++ Assets/Editor/EditorTools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class EditorTools : Editor {
+
+	[MenuItem("Tool/SetAssetBundleName")]
+    static void SetAssetBundleName()
+    {
+        string[] strs = Selection.assetGUIDs;
+        string str = AssetDatabase.GUIDToAssetPath(strs[0]);
+        string _DirName = Path.GetFileName(str);
+        Debug.Log(_DirName);
+        var files = Directory.GetFiles(str);
+        foreach (var file in files)
+        {
+            if (file.Contains(".meta"))
+                continue;
+
+            AssetImporter asset = AssetImporter.GetAtPath(file);
+            asset.assetBundleName = _DirName;
+            asset.SaveAndReimport();
+        }
+        AssetDatabase.Refresh();
+    }
+}
